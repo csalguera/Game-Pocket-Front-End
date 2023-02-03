7b6791aaeeeea3c5c5b17c4392f77d07e495335f
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-03 13:24:38 -0500

    import useState and add state to FriendList page

--- src/pages/FriendList/FriendList.jsx
+++ src/pages/FriendList/FriendList.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import Friends from "../../components/Friends/Friends"
 
 const FriendList = () => {
+  const [friends, setFriends] = useState([])
+  
   return (
     <>
       <h1>Friends</h1>
