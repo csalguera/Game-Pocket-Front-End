414f62126de24ba903a9cef6c24632705d22a89d
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-03 13:33:32 -0500

    definte state in App and import useEffect to allow state to change

--- src/pages/FriendList/FriendList.jsx
+++ src/pages/FriendList/FriendList.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react"
 import Friends from "../../components/Friends/Friends"
 
 const FriendList = () => {
-  const [friends, setFriends] = useState([])
-  
   return (
     <>
       <h1>Friends</h1>
