9fd91b2048edd54143968d9a9cb1359261662ec0
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-04 12:59:28 -0500

    wrap div element around ul elemnet in Lobby component

--- src/components/Lobby/Lobby.jsx
+++ src/components/Lobby/Lobby.jsx
@@ -3,12 +3,14 @@ const Lobby = ({ lobbies, user }) => {
     <>
       <h1>This is the lobby component</h1>
       {lobbies.map(lobby => (
-        <ul>
-          <li>Name: {lobby.name}</li>
-          <li>Content: {lobby.content}</li>
-          <li>Gamerooms: {lobby.gamerooms}</li>
-          <li>Members: {lobby.members}</li>
-        </ul>
+        <div>
+          <ul>
+            <li>Name: {lobby.name}</li>
+            <li>Content: {lobby.content}</li>
+            <li>Gamerooms: {lobby.gamerooms}</li>
+            <li>Members: {lobby.members}</li>
+          </ul>
+        </div>
       ))}
     </>
   )
