7e0fd750edb350bc299ed80e9d981f159e5c7b05
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-04 14:08:47 -0500

    add idx parameter to map of lobbies, add h2 and h3 tags

--- src/components/Lobby/Lobby.jsx
+++ src/components/Lobby/Lobby.jsx
@@ -1,14 +1,15 @@
 const Lobby = ({ lobbies, user }) => {
   return (
     <>
-      <h1>This is the lobby component</h1>
-      {lobbies.map(lobby => (
+      <h1>These are the available lobbies</h1>
+      {lobbies.map((lobby, idx) => (
         <div>
           <ul>
-            <li>Name: {lobby.name}</li>
-            <li>Content: {lobby.content}</li>
-            <li>Gamerooms: {lobby.gamerooms}</li>
-            <li>Members: {lobby.members}</li>
+            <li><h2>Lobby {idx + 1}</h2></li>
+            <li><h3>Name: {lobby.name}</h3></li>
+            <li><h3>Content: {lobby.content}</h3></li>
+            <li><h3>Gamerooms: {lobby.gamerooms}</h3></li>
+            <li><h3>Members: {lobby.members}</h3></li>
           </ul>
         </div>
       ))}
