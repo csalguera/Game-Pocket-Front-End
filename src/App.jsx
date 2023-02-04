0ea6545212bcf4a8c61a4db8eceb97f49e74b811
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-04 12:38:27 -0500

    prop drill lobbies state to Landing page and destructure it

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ const App = () => {
     <>
       <NavBar user={user} handleLogout={handleLogout} />
       <Routes>
-        <Route path="/" element={<Landing user={user} />} />
+        <Route path="/" element={<Landing lobbies={lobbies} user={user} />} />
         <Route
           path="/signup"
           element={<Signup handleSignupOrLogin={handleSignupOrLogin} />}
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 import styles from './Landing.module.css'
 import Lobby from '../../components/Lobby/Lobby'
 
-const Landing = ({ user }) => {
+const Landing = ({ user, lobbies }) => {
   return (
     <main className={styles.container}>
       <h1>Hello, {user ? user.name : 'please log in or sign up to access the lobby'}</h1>
