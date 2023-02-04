a2ca9e1195ed3053afc346244086b95f9799acc1
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-04 16:02:48 -0500

    import lobbyService

--- src/pages/LobbyRoom/LobbyRoom.jsx
+++ src/pages/LobbyRoom/LobbyRoom.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+// Services
+import * as lobbyService from '../../services/lobbyService'
+
 const LobbyRoom = ({ user }) => {
   return (
     <>
