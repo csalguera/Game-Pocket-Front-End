bc51e21ff257038fc3d0514a53e3440d4be63fde
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-05 18:46:33 -0500

    map through records and display various data associated with each

--- src/components/Ranking/Ranking.jsx
+++ src/components/Ranking/Ranking.jsx
@@ -7,6 +7,15 @@ const Ranking = ({ records, user }) => {
         <li>Second Place</li>
         <li>Third Place</li>
       </ol>
+      {records.map(record => (
+        <div key={record._id} >
+          <p>{record.owner}</p>
+          <p>Game: {record.game}</p>
+          <p>Wins: {record.win}</p>
+          <p>Losses: {record.loss}</p>
+          <p>Win Rate: {Math.floor(record.win / (record.win + record.loss) * 100)}%</p>
+        </div>
+      ))}
     </div>
   );
 }
