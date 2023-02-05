c3771d1e721044abb66caacadc729f7c79bd3ba1
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-05 13:52:23 -0500

    stlye fonts

--- src/pages/ChangePassword/ChangePassword.jsx
+++ src/pages/ChangePassword/ChangePassword.jsx
@@ -16,7 +16,7 @@ const ChangePassword = props => {
         <h1>C h a n g e<br></br>P a s s w o r d</h1>
         <img src="https://i.imgur.com/ArfuBsE.png" alt="Simpsons Family" />
       </div>
-      <main className="body">
+      <main className="body" id="simpsons">
         <div className='form-container'>
           <p>{message}</p>
           <ChangePasswordForm {...props} updateMessage={updateMessage} />
