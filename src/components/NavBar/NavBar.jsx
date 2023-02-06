da694808af99b4df5f5769024f0403eac3b4a239
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-05 21:12:01 -0500

    add flexwrap to media query

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,12 +5,12 @@ const NavBar = ({ user, handleLogout }) => {
     <nav className="navbar">
       {user ?
         <ul >
-          <li>Welcome, {user.name}</li>
+          <li id="welcome">Welcome, {user.name}</li>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/friends">Friends</Link></li>
           <li><Link to="/profiles">Profiles</Link></li>
           <li><Link to="/leaderboard">Leaderboard</Link></li>
-          <li><Link to="/change-password">Change Password</Link></li>
+          <li><Link to="/change-password" id="change-password">Change Password</Link></li>
           <li><Link to="" onClick={handleLogout}>Log Out</Link></li>
         </ul>
       :
