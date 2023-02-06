2819a414d540fc5079acdc302a9d2691fd63f236
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-06 09:29:07 -0500

    implement functionality to NavStick component

--- src/components/NavStick/NavStick.jsx
+++ src/components/NavStick/NavStick.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
 const NavStick = () => {
+  const [route, setRoute] = useState(0)
+
+  const routes = [
+    '/',
+    '/friends',
+    '/profiles'
+  ]
+
+  const destination = `${routes[route]}`
+
+  function navstickRight() {
+    route >= routes.length - 1 ? setRoute(0) : setRoute(route + 1)
+  }
+
+  console.log(destination)
+
   return (
     <>
+      <button onClick={navstickRight}>
+        Right
+      </button>
+      <Link to={destination}>
+        <button>Navigate</button>
+      </Link>
     </>
   )
 }
