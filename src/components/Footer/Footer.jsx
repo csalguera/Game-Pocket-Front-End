c528f64f3aa996148c92e1c5d9192c6dc2e24562
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-06 10:31:00 -0500

    add attributes to a tags to prevent linting

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import NavStick from "../NavStick/NavStick"
+import styles from './Footer.module.css'
 
 const Footer = ({ user }) => {
   if (user)
   return (
     <>
       <NavStick />
+      <div className={styles.linkContainer}>
+        <a href="https://github.com/WarmSkin/Game-Pocket-Front-End" target="_blank" rel="noreferrer">GitHub</a>
+        <p>|</p>
+        <a href="/" target="_blank" rel="noreferrer">Attributions</a>
+      </div>
     </>
   )
 }
