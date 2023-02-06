bbf275ad233684a9157d69ea99de8737d61df8ad
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-06 13:20:29 -0500

    apply CSS rules from module to index.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,10 @@
 import NavStick from "../NavStick/NavStick"
-import styles from './Footer.module.css'
 
 const Footer = ({ user }) => {
   return (
     <>
       <NavStick user={user} />
-      <div className={styles.linkContainer}>
+      <div className="footer-container">
         <a href="https://github.com/WarmSkin/Game-Pocket-Front-End" target="_blank" rel="noreferrer">GitHub</a>
         <p>|</p>
         {/* link in attributions must be changed once README is done */}
