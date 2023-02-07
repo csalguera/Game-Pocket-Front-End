e7e6b20d00741807a2e270f986bb6f0432dd0111
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-07 12:03:33 -0500

    change styling for NavStick component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,15 +2,17 @@ import NavStick from "../NavStick/NavStick"
 
 const Footer = ({ user }) => {
   return (
-    <div className="footer">
+    <>
       <NavStick user={user} />
-      <div className="footer-container">
-        <a href="https://github.com/WarmSkin/Game-Pocket-Front-End" target="_blank" rel="noreferrer">GitHub</a>
-        <p>|</p>
-        {/* link in attributions must be changed once README is done */}
-        <a href="/" target="_blank" rel="noreferrer">Attributions</a>
+      <div className="footer">
+        <div className="footer-container">
+          <a href="https://github.com/WarmSkin/Game-Pocket-Front-End" target="_blank" rel="noreferrer">GitHub</a>
+          <p>|</p>
+          {/* link in attributions must be changed once README is done */}
+          <a href="/" target="_blank" rel="noreferrer">Attributions</a>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
