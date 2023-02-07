7829978d7ceac0810d3d29142965e84f2e114631
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-07 14:01:00 -0500

    add conditional to display Footer component

--- src/components/NavStick/NavStick.jsx
+++ src/components/NavStick/NavStick.jsx
@@ -22,7 +22,6 @@ const NavStick = ({ user }) => {
     route >= routes.length - 1 ? setRoute(0) : setRoute(route + 1)
   }
 
-  if (user)
   return (
     <div className="navstick-container">
       <div className="buttons-container">
