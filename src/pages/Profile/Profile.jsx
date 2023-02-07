28ef5a62bd0d6035e4e40b93e6c7d82736f4b448
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-06 21:41:02 -0500

    fetch profile through service

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import * as profileService from '../../services/profileService'
 
 const Profile = () => {
   const { id } = useParams()
-  console.log(id)
+  const [profile, setProfile] = useState('')
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const data = await profileService.getProfile(id)
+      setProfile(data)
+    }
+    fetchProfile()
+  }, [id])
+  
+  console.log(profile)
   return (
     <>
     </>
