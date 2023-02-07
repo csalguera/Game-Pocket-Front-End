e9d32489d942c0261dfdad902963eaf7b3d65bfa
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-06 21:56:50 -0500

    destructure user

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import * as profileService from '../../services/profileService'
 
-const Profile = () => {
+const Profile = ({ user }) => {
   const { id } = useParams()
   const [profile, setProfile] = useState('')
 
