78b8a5d82b3b77e2e6b6a1f31f2877f3d33c4fec
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-08 12:58:29 -0500

    add containers to allow for styling individual elements

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -3,13 +3,17 @@ const Message = ({ user, message, deleteMessage }) => {
     <>
       {message.sender === user.profile ? 
       <div id="my-message">
-        {message.content} - {message.from}
+        <div className="message-content">
+          {message.content} - {message.from}
+        </div>
         <button onClick={() => deleteMessage(message._id)}>X</button>
       </div>
       
       :
       <div id="other-message">
+        <div className="message-content">
         {message.content} - {message.from}
+        </div>
       </div>
       }
     </>
