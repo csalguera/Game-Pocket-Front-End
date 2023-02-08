9a43a038e30c71cbb36fbbbdaf1373904dd9101d
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-08 12:50:17 -0500

    update layout for chatroom

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,13 +1,16 @@
 const Message = ({ user, message, deleteMessage }) => {
   return (
     <>
-      <div>
+      {message.sender === user.profile ? 
+      <div id="my-message">
+        {message.content} - {message.from}
+        <button onClick={() => deleteMessage(message._id)}>X</button>
+      </div>
+      
+      :
+      <div id="other-message">
         {message.content} - {message.from}
       </div>
-      {message.sender === user.profile ? 
-        <button onClick={() => deleteMessage(message._id)}>Delete</button>
-        :
-        ""
       }
     </>
   )
