2391148a11a4fb92fac525534d2df3c0fcc65464
Pin Chen <pamyc1876@gmail.com>
2023-02-08 17:31:13 -0500

    Remove friends page, and the link on the Navbar.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,6 @@ const NavBar = ({ user, handleLogout }) => {
         <ul >
           <li id="welcome">Welcome, {user.name}</li>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/friends">Friends</Link></li>
           <li><Link to="/profiles">Profiles</Link></li>
           <li><Link to="/leaderboard">Leaderboard</Link></li>
           <li><Link to="/my-page">My Page</Link></li>
