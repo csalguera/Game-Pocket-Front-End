3ae29d7b2b33aa2e2155c22286329a4c8906d749
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-08 09:42:19 -0500

    add code for message form

--- src/components/MessageForm/MessageForm.jsx
+++ src/components/MessageForm/MessageForm.jsx
@@ -1,6 +1,24 @@
 const MessageForm = ({ handleSendMessage, handleChange, message }) => {
   return (
     <>
+      <form
+        autoComplete='off'
+        onSubmit={handleSendMessage}
+      >
+      <div>
+        <label htmlFor="content">Message</label>
+        <input 
+          type="text"
+          name='content'
+          autoComplete="off"
+          onChange={handleChange}
+          value={message.content}
+        />
+      </div>
+      <div>
+        <button>Send</button>
+      </div>
+      </form>
     </>
   )
 }
