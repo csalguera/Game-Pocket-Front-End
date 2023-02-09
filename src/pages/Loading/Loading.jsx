bd47422d30e3eb23f22198ac59fd72da587b371b
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-09 13:55:27 -0500

    add loading page

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -2,8 +2,6 @@ const Loading = () => {
   return (
     <>
     <div id="loading">
-      <h1>LOADING</h1>
-      <img src="https://i.imgur.com/1VrS6Xk.gif" alt="Rotating arcade machine" />
     </div>
     </>
   )
