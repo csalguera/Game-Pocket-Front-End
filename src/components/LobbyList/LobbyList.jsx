ec751d1f82583aad5aa3e917204665a45af08b6a
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-09 13:28:52 -0500

    import useNavigate

--- src/components/LobbyList/LobbyList.jsx
+++ src/components/LobbyList/LobbyList.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import * as lobbyService from '../../services/lobbyService'
 
 const LobbyList = ({ user, socket }) => {
-
+  const navigate = useNavigate()
   const [lobbies, setLobbies] = useState([])
   const [refresh, setRefresh] = useState(0)
   const [formData, setFormData] = useState({
