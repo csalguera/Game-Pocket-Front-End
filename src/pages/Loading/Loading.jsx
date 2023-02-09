68644fcedf2a4aa8ab49c416fd2d249b7cc2a7b3
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-09 13:52:31 -0500

    create loading page

---
+++ src/pages/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <>
+    <div id="loading">
+      <h1>LOADING</h1>
+      <img src="https://i.imgur.com/1VrS6Xk.gif" alt="Rotating arcade machine" />
+    </div>
+    </>
+  )
+}
+
+export default Loading
