d1ee6f89be99b0e18cb24f684a81ae6ab2f19140
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-08 19:35:22 -0500

    style landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,20 @@
 import styles from './Landing.module.css'
 import LobbyList from '../../components/LobbyList/LobbyList'
+import { Link } from 'react-router-dom'
 
 const Landing = ({ user, socket }) => {
   return (
     <main className={styles.container} id="space-invaders">
-      {user ? <LobbyList user={user} socket={socket} /> : ''}
+      {user ? <LobbyList user={user} socket={socket} /> 
+      :
+      <div id="game-over">
+        <h1>GAME OVER</h1>
+        <div id="options">
+          <Link to="/login">Log In</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      </div>
+      }
     </main>
   )
 }
