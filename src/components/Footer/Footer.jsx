532e63a8dbdf7fdfd51c947c35483f1405159058
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-10 10:39:11 -0500

    add link to attributions in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,7 @@ const Footer = ({ user, handleLogout }) => {
         <div className="footer-container">
           <a href="https://github.com/WarmSkin/Game-Pocket-Front-End" target="_blank" rel="noreferrer">GitHub</a>
           <p>|</p>
-          {/* link in attributions must be changed once README is done */}
-          <a href="/" target="_blank" rel="noreferrer">Attributions</a>
+          <a href="https://github.com/csalguera/Game-Pocket-Front-End/blob/main/attributions.md" target="_blank" rel="noreferrer">Attributions</a>
         </div>
       </div>
     </>
